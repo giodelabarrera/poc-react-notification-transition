1746237c5e080e251d84634b72abde4f682e50ab
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-02-04 17:19:51 +0100

    Add stable transition

--- src/App.js
+++ src/App.js
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
-import { Button } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
 import "./styles.css";
 
-export default function App() {
-  const [state, setState] = React.useState(true);
+const NOTIFICATION_TYPE = {
+  LIKE: "like",
+  COMMENT: "comment",
+};
+
+const notifications = [
+  { id: 1, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 0" },
+  { id: 2, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 1" },
+  { id: 3, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 2" },
+  { id: 4, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 3" },
+  { id: 5, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 4" },
+  { id: 6, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 5" },
+  { id: 7, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 6" },
+  { id: 8, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 7" },
+  { id: 9, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 8" },
+  { id: 10, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 9" },
+  { id: 11, type: NOTIFICATION_TYPE.LIKE, message: "Lorem ipsum 10" },
+  { id: 12, type: NOTIFICATION_TYPE.COMMENT, message: "Lorem ipsum 11" },
+];
+
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevCurrentIndex) => prevCurrentIndex + 1);
+  };
+
+  const notification = notifications[currentIndex];
+
   return (
-    <>
-      <div className="main">
-        <SwitchTransition>
-          <CSSTransition
-            key={state}
-            addEndListener={(node, done) => {
-              node.addEventListener("transitionend", done, false);
-            }}
-            classNames="fade"
-          >
-            <div className="button-container">
-              <Button
-                className="btn"
-                onClick={() => setState((state) => !state)}
-              >
-                {state ? "Hello, world!" : "Goodbye, world!"}
-              </Button>
-            </div>
-          </CSSTransition>
-        </SwitchTransition>
-      </div>
-    </>
+    <div className="app">
+      <main>
+        <Header>
+          <SwitchTransition>
+            <CSSTransition
+              key={currentIndex}
+              addEndListener={(node, done) => {
+                node.addEventListener("transitionend", done, false);
+              }}
+              classNames="fade"
+            >
+              <div className="item-container">
+                <Item message={notification.message} />
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
+        </Header>
+
+        <Button onClick={handleNextClick}>Next</Button>
+      </main>
+    </div>
   );
 }
+
+function Header({ children }) {
+  return <header>{children}</header>;
+}
+
+function Item({ message }) {
+  return <p className="item">{message}</p>;
+}
+
+export default App;
