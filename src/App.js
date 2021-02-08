e05663dce5079d43a89d0e5357de16c42cb89475
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-02-08 09:21:12 +0100

    Add transition wrapper

--- src/App.js
+++ src/App.js
@@ -37,47 +37,11 @@ function App() {
     <div className="app">
       <main>
         <Header>
-          <SwitchTransition>
-            <CSSTransition
-              key={currentIndex}
-              addEndListener={(node, done) => {
-                node.addEventListener("transitionend", done, false);
-              }}
-              classNames="fade"
-            >
-              <div className="item-container">
-                <div className="item">
-                  <span className="header__icon">
-                    <svg
-                      fill="blue"
-                      className="header__icon-svg"
-                      viewBox="0 0 24 24"
-                    >
-                      {type === NOTIFICATION_TYPE.LIKE && (
-                        <path
-                          fillRule="evenodd"
-                          d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
-                        ></path>
-                      )}
-                      {type === NOTIFICATION_TYPE.COMMENT && (
-                        <path
-                          fillRule="evenodd"
-                          d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
-                        ></path>
-                      )}
-                    </svg>
-                    <span className="header__icon-first-wave"></span>
-                    <span className="header__icon-second-wave"></span>
-                  </span>
-                  <div className="header__inner">
-                    <div className="header__title">
-                      <p>{message}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </CSSTransition>
-          </SwitchTransition>
+          <SecondItem
+            type={type}
+            message={message}
+            currentIndex={currentIndex}
+          />
         </Header>
 
         <Button onClick={handleNextClick}>Next</Button>
@@ -90,34 +54,88 @@ function Header({ children }) {
   return <header className="header">{children}</header>;
 }
 
-// function Item({ type, message }) {
-//   return (
-//     <div className="item">
-//       <span className="header__icon">
-//         <svg fill="blue" className="header__icon-svg" viewBox="0 0 24 24">
-//           {type === NOTIFICATION_TYPE.LIKE && (
-//             <path
-//               fillRule="evenodd"
-//               d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
-//             ></path>
-//           )}
-//           {type === NOTIFICATION_TYPE.COMMENT && (
-//             <path
-//               fillRule="evenodd"
-//               d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
-//             ></path>
-//           )}
-//         </svg>
-//         <span className="header__icon-first-wave"></span>
-//         <span className="header__icon-second-wave"></span>
-//       </span>
-//       <div className="header__inner">
-//         <div className="header__title">
-//           <p>{message}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+function SecondItem({ type, message, currentIndex }) {
+  return (
+    <div className="item">
+      <SwitchTransition>
+        <CSSTransition
+          key={`${currentIndex}-icon`}
+          addEndListener={(node, done) => {
+            node.addEventListener("transitionend", done, false);
+          }}
+          classNames="fade"
+        >
+          <div>
+            <div className="header__icon">
+              <svg fill="blue" className="header__icon-svg" viewBox="0 0 24 24">
+                {type === NOTIFICATION_TYPE.LIKE && (
+                  <path
+                    fillRule="evenodd"
+                    d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
+                  ></path>
+                )}
+                {type === NOTIFICATION_TYPE.COMMENT && (
+                  <path
+                    fillRule="evenodd"
+                    d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
+                  ></path>
+                )}
+              </svg>
+              <span className="header__icon-first-wave"></span>
+              <span className="header__icon-second-wave"></span>
+            </div>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+      <SwitchTransition>
+        <CSSTransition
+          key={`${currentIndex}-title`}
+          addEndListener={(node, done) => {
+            node.addEventListener("transitionend", done, false);
+          }}
+          classNames="fade"
+        >
+          <div>
+            <div className="header__inner">
+              <div className="header__title">
+                <p>{message}</p>
+              </div>
+            </div>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </div>
+  );
+}
+
+function Item({ type, message }) {
+  return (
+    <div className="item">
+      <span className="header__icon">
+        <svg fill="blue" className="header__icon-svg" viewBox="0 0 24 24">
+          {type === NOTIFICATION_TYPE.LIKE && (
+            <path
+              fillRule="evenodd"
+              d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
+            ></path>
+          )}
+          {type === NOTIFICATION_TYPE.COMMENT && (
+            <path
+              fillRule="evenodd"
+              d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
+            ></path>
+          )}
+        </svg>
+        <span className="header__icon-first-wave"></span>
+        <span className="header__icon-second-wave"></span>
+      </span>
+      <div className="header__inner">
+        <div className="header__title">
+          <p>{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
