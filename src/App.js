ad2c10140009d8118e20b8552d67652f728e387d
ramonmcros <ramonmcros@gmail.com>
2021-03-05 11:22:59 +0200

    Add next icon animation

--- src/App.js
+++ src/App.js
@@ -96,33 +96,36 @@ function HeaderContent({ type, message, currentIndex, navDirection }) {
         "header__content--is-prev": navDirection === NAVIGATION_DIRECTION.PREV,
       })}
     >
-      <TransitionGroup className="header__inner">
+      <TransitionGroup className="header__icon">
         <CSSTransition
           key={`${currentIndex}-icon`}
-          addEndListener={(node, done) => {
-            node.addEventListener("transitionend", done, false);
+          // addEndListener={(node, done) => {
+          //   node.addEventListener("transitionend", done, false);
+          // }}
+          timeout={{
+            appear: 0,
+            enter: 850,
+            exit: 850,
           }}
           classNames="fade-icon"
         >
           <div>
-            <div className="header__icon">
-              <svg fill="blue" className="header__icon-svg" viewBox="0 0 24 24">
-                {type === NOTIFICATION_TYPE.LIKE && (
-                  <path
-                    fillRule="evenodd"
-                    d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
-                  ></path>
-                )}
-                {type === NOTIFICATION_TYPE.COMMENT && (
-                  <path
-                    fillRule="evenodd"
-                    d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
-                  ></path>
-                )}
-              </svg>
-              <span className="header__icon-first-wave"></span>
-              <span className="header__icon-second-wave"></span>
-            </div>
+            <svg fill="blue" className="header__icon-svg" viewBox="0 0 24 24">
+              {type === NOTIFICATION_TYPE.LIKE && (
+                <path
+                  fillRule="evenodd"
+                  d="M12.001 19c-1.106 0-6.507-4.06-7.72-7.952A4.522 4.522 0 0 1 4 9.502C4 7.02 6.03 5 8.527 5 9.875 5 11.14 5.594 12 6.61A4.536 4.536 0 0 1 15.47 5C17.967 5 20 7.02 20 9.502c0 .504-.09 1.01-.268 1.504-1.197 4.011-6.57 7.994-7.73 7.994z"
+                ></path>
+              )}
+              {type === NOTIFICATION_TYPE.COMMENT && (
+                <path
+                  fillRule="evenodd"
+                  d="M5 7.173C5 5.973 5.897 5 7.006 5h9.988C18.102 5 19 5.982 19 7.173v7.654c0 1.2-.897 2.173-2.006 2.173H7.006C5.898 17 5 16.018 5 14.827V7.173zM8 17h5l-5 3v-3z"
+                ></path>
+              )}
+            </svg>
+            <span className="header__icon-first-wave"></span>
+            <span className="header__icon-second-wave"></span>
           </div>
         </CSSTransition>
       </TransitionGroup>
